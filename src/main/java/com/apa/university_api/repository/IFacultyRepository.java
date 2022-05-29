cc7ea5b587534ff18192860a381d86c4df57df2d
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-05-29 15:33:15 +0430

    just the required annotation added

--- src/main/java/com/apa/university_api/repository/IFacultyRepository.java
+++ src/main/java/com/apa/university_api/repository/IFacultyRepository.java
@@ -2,6 +2,8 @@ package com.apa.university_api.repository;
 
 import com.apa.university_api.model.Faculty;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IFacultyRepository extends JpaRepository<Faculty, Long> {
 }
