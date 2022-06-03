4961cab0d43a79706247c8ab7cc38f4382b7f938
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-06-03 18:52:53 +0430

    inserting new student endpoint added to the studnet controller

---
+++ src/main/java/com/apa/university_api/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.apa.university_api.controller;
+
+import com.apa.university_api.model.Response;
+import com.apa.university_api.model.Student;
+import com.apa.university_api.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Response> add(@RequestBody Student student) {
+        Response result = this.studentService.add(student);
+        if (result.getResultCode() == 200)
+            return ResponseEntity.ok(result);
+        return ResponseEntity.badRequest().body(result);
+    }
+}
--- src/main/java/com/apa/university_api/service/StudentService.java
+++ src/main/java/com/apa/university_api/service/StudentService.java
@@ -1,7 +1,27 @@
 package com.apa.university_api.service;
 
+import com.apa.university_api.model.Response;
+import com.apa.university_api.model.Student;
+import com.apa.university_api.repository.IStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class StudentService {
+    private final IStudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(IStudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Response add(Student student) {
+        Optional<Student> isStudentExists = this.studentRepository.findById(student.getStudentNumber());
+        if (isStudentExists.isPresent())
+            return new Response(400, "This student exists can't add it again!", null);
+        Student savedStudent = this.studentRepository.save(student);
+        return new Response(200, "Student added successfully!", savedStudent);
+    }
 }
