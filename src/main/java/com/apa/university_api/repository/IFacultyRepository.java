aaa795d7b78d61129b111d1ef20bdfe5c468b988
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-07-29 11:45:27 +0430

    somw final changes in implementing pagination

--- src/main/java/com/apa/university_api/repository/IFacultyRepository.java
+++ src/main/java/com/apa/university_api/repository/IFacultyRepository.java
@@ -1,12 +1,12 @@
 package com.apa.university_api.repository;
 
 import com.apa.university_api.model.Faculty;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface IFacultyRepository extends JpaRepository<Faculty, Long> {
+public interface IFacultyRepository extends PagingAndSortingRepository<Faculty, Long> {
     Optional<Faculty> findByName(String facultyName);
 }
--- src/main/java/com/apa/university_api/repository/ITeacherRepository.java
+++ src/main/java/com/apa/university_api/repository/ITeacherRepository.java
@@ -1,8 +1,7 @@
 package com.apa.university_api.repository;
 
 import com.apa.university_api.model.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ITeacherRepository extends JpaRepository<Teacher, String> {
-
+public interface ITeacherRepository extends PagingAndSortingRepository<Teacher, String> {
 }
